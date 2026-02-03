ca1474c3371c6ac3f76d8551ceefcc51b4d785e3
Herdis Heida <herdisj24@ru.is>
2026-02-03 13:02:27 +0000

    test for class token

---
+++ myTest.py
@@ -0,0 +1,24 @@
+from ltoken import LToken
+
+def test_class_token():
+    print(LToken.ID)
+    print(LToken.ASSIGN)
+    print(LToken.SEMICOL)
+    print(LToken.INT)
+    print(LToken.PLUS)
+    print(LToken.MINUS)
+    print(LToken.MULT)
+    print(LToken.LPAREN)
+    print(LToken.RPAREN)
+    print(LToken.PRINT)
+    print(LToken.END)
+    print(LToken.ERROR)
+    return
+
+
+def main():
+
+    test_class_token()
+
+
+main()
