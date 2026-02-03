177a97c3810934df929ea514643de77a7d1dc503
LAPTOP-PHSLA1FS\julia <juliagunnlaugs05@gmail.com>
2026-02-03 12:56:21 +0000

    token constants

--- ltoken.py
+++ ltoken.py
@@ -1,3 +1,17 @@
 class LToken():
+
+    ID = 0
+    ASSIGN = 1
+    SEMICOL = 2
+    INT = 3
+    PLUS = 4
+    MINUS = 5
+    MULT = 6
+    LPAREN = 7
+    RPAREN = 8
+    PRINT = 9
+    END = 10
+    ERROR = 11
+
     def __init__(self):
         pass
