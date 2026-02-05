7be6ae71198da64e0779910b580dec010bdaecf4
Herdis Heida <herdisj24@ru.is>
2026-02-05 11:12:34 +0000

    parse correc t order

--- lparser.py
+++ lparser.py
@@ -7,6 +7,9 @@ class LParser:
         self.lexer = lexer
         self.curr_token = None
 
+        self.operators = []
+        self.variables = []
+
     def parse(self):
         self.next_token()
         self.statements()
@@ -21,6 +24,26 @@ class LParser:
         print ("Syntax Error")
         exit(1)
 
+
+    def check_intermediate_line(self):
+        """ Put operators and variable toknes into respective lists """
+        if  self.curr_token.is_operator():
+            self.operators.insert(0, self.curr_token)
+        elif self.curr_token.is_variable():
+            self.variables.append(self.curr_token)
+        elif self.curr_token.token_code == LToken.PRINT:
+            self.operators.insert(0, self.curr_token)
+
+    def print_intermediate_line(self):
+        """ Print the intermediate line  """
+        for var in self.variables:
+            print("PUSH ", var, end='\n')
+        for op in self.operators:
+            print(op, end='\n')
+        self.variables = []
+        self.operators = []
+
+
     def statements(self):
         """ start symbol,
         Statements -> Statement ; Statements | end
@@ -28,7 +51,6 @@ class LParser:
 
         # end
         if self.curr_token.token_code == LToken.END:
-            print(self.curr_token)
             return self.next_token() # parse the end
 
         # Statement ; Statement
@@ -37,8 +59,11 @@ class LParser:
         if self.curr_token.token_code != LToken.SEMICOL:
             return self.error()
             
-        print(self.curr_token)
+        self.check_intermediate_line()
         self.next_token() # parse SEMiCoL
+
+        self.print_intermediate_line()
+
         return self.statements()
         
 
@@ -46,20 +71,20 @@ class LParser:
         """ Statement -> id = Expr | print id """
         # statement starts with id or print, so we can check the current token to decide which production to use
         if self.curr_token.token_code == LToken.ID:
-            print(self.curr_token)
+            self.check_intermediate_line()
             self.next_token() # consume id
             if self.curr_token.token_code != LToken.ASSIGN:
                 return self.error() # dumymy error function
-            print(self.curr_token)
+            self.check_intermediate_line()
             self.next_token() # consume =
             self.expr()
 
         elif self.curr_token.token_code == LToken.PRINT:
-            print(self.curr_token)
+            self.check_intermediate_line()
             self.next_token() # consume print
             if self.curr_token.token_code != LToken.ID:
                 return self.error()
-            print(self.curr_token)
+            self.check_intermediate_line()
             self.next_token() # consume id
             
         else:
@@ -73,12 +98,12 @@ class LParser:
         self.term()
 
         if self.curr_token.token_code == LToken.PLUS:
-            print(self.curr_token)
+            self.check_intermediate_line()
             self.next_token() # parse +
             self.expr()
 
         elif self.curr_token.token_code == LToken.MINUS:
-            print(self.curr_token)
+            self.check_intermediate_line()
             self.next_token() # parse -
             self.expr()
 
@@ -91,7 +116,7 @@ class LParser:
         self.factor()
 
         if self.curr_token.token_code == LToken.MULT:
-            print(self.curr_token)
+            self.check_intermediate_line()
             self.next_token() # parse *
             self.term()
 
@@ -99,20 +124,20 @@ class LParser:
     def factor(self):
         """ Factor -> int | id | ( Expr ) """
         if self.curr_token.token_code == LToken.INT:
-            print(self.curr_token)
+            self.check_intermediate_line()
             self.next_token() # parse int
 
         elif self.curr_token.token_code == LToken.ID:
-            print(self.curr_token)
+            self.check_intermediate_line()
             self.next_token() # parse id
 
         elif self.curr_token.token_code == LToken.LPAREN:
-            print(self.curr_token)
+            self.check_intermediate_line()
             self.next_token() # parse (
             self.expr()
             if self.curr_token.token_code != LToken.RPAREN:
                 return self.error()
-            print(self.curr_token)
+            self.check_intermediate_line()
             self.next_token() # parse )
         
         else:
--- ltoken.py
+++ ltoken.py
@@ -18,4 +18,11 @@ class LToken():
         self.token_code = token
 
     def __str__(self):
-        return f"Token: {self.token_code} Lexeme: {self.lexeme}"
+        # return f"Token: {self.token_code} Lexeme: {self.lexeme}"
+        return f"{self.lexeme}"
+    
+    def is_operator(self):
+        return self.token_code in {LToken.PLUS, LToken.MINUS, LToken.MULT, LToken.ASSIGN}
+    
+    def is_variable(self):
+        return self.token_code in {LToken.ID, LToken.INT}
