8052c209c9a74ee27f29108a777caac81c1b83c9
julia.gunnlaugsd <juliagunnlaugs05@gmail.com>
2026-02-05 08:41:36 +0000

    parser class basics

--- lparser.py
+++ lparser.py
@@ -1,2 +1,17 @@
-def LParser():
-    return # TODO
+from ltoken import LToken
+
+class LParser:
+
+    def LParser():
+        # recursive descent for grammar G
+        return # TODO
+
+    def parse(self):
+        self.next_token()
+        self.statements()
+        print() # Make sure the intermediate code ends with a newline
+
+    def next_token(self):
+        self.curr_token = self.lexer.get_next_token()
+        if self.curr_token.token_code == LToken.ERROR:
+            self.error()
