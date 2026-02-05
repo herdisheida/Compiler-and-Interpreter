c5d9a829cc00ee8156e38d5499230c935ea957c9
Herdis Heida <herdisj24@ru.is>
2026-02-05 10:31:22 +0000

    error handling is interpretrer

--- sinterpreter.py
+++ sinterpreter.py
@@ -2,7 +2,12 @@ class SInterpreter:
     def cycle(self):
         pass
 
+
+    def error(self, name_of_operator):
+        print(f"Error for operator: {name_of_operator}")
+        exit(1)
+
     
-def main(self):
+def main():
     interpreter = SInterpreter()
     interpreter.cycle()
