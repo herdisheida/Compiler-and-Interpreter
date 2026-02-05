26a1867c266386b7d3c5ddc096fe727398c3fbd8
Herdis Heida <herdisj24@ru.is>
2026-02-05 10:40:33 +0000

    draft

--- sinterpreter.py
+++ sinterpreter.py
@@ -1,11 +1,21 @@
+import sys
+
 class SInterpreter:
     def __init__(self):
         self.stack = []
         self.variables = {}
 
+    def get_next_line(self):
+        return sys.stdin.readline().strip()
 
     def cycle(self):
-        pass
+        line = self.get_next_line()
+        while line:
+            self.process_line(line)
+            
+
+    def process_line(self, line):
+        x, token, y, lexeme = line.split()
 
 
     def error(self, name_of_operator):
